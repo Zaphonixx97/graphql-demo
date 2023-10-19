d69f32fa77be678937225c40af8516aca48d8807
caunicandrei <zaphonixx@yahoo.com>
2023-10-19 21:54:41 +0300

    add mysql db connection and cleanup Book model

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -28,14 +28,4 @@ public class Book {
         this.publicationYear = publicationYear;
         this.author = author;
     }
-
-    public static List<Book> books = Arrays.asList(
-            new Book(1L, "Harry Potter and the Philosopher's Stone", 223, new Author()),
-            new Book(2L, "Moby Dick", 635, new Author()),
-            new Book(3L, "Interview with the vampire", 371, new Author())
-    );
-
-    public static Book getById(Long id) {
-        return books.stream().filter(book -> book.getId().equals(id)).findFirst().orElse(null);
-    }
 }
